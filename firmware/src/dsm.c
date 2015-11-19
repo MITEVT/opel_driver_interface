c6bdc78860ff4f62ecb22bfb5702cf82ca24a11c
Bryson <bgalapon@mit.edu>
2015-11-19 02:01:16 -0500

    updated dsm state machine

--- firmware/src/dsm.c
+++ firmware/src/dsm.c
@@ -3,24 +3,36 @@
 MODES_T mode;
 
 void DSM_Init(void){
-	mode = OFF;
+                    //Check Skanda's edit of types.h
+	mode = MODE_OFF;
 }
 
 
-ERROR_T DSM_Step(INPUT_T *input, STATE_T *state, OUTPUT_T *output, MODE_REQUEST_T mode_request){
-	if(mode_request == REQ_OFF){
-		if(mode != OFF){
+ERROR_T DSM_Step(INPUT_T *input, STATE_T *state, OUTPUT_T *output){
+	if(input->keymodes == KEYMODE_OFF){
+		if(mode != MODE_OFF){
 			//Move to OFF
-			if(mode == ACCESSORIES){
-				mode = SHUTDOWN;
-				return AccStep(input,output);
-			}else if(mode == CHARGE){
-				mode = SHUTDOWN;
+			if(mode == MODE_ACCESSORIES){
+				mode = MODE_SHUTDOWN;
+				return AccStep(input, output, state, REQ_SHUTDOWN);
+			}else if(mode == MODE_CHARGE){
+				mode = MODE_SHUTDOWN;
+				return ChargeStep(input, output, state, REQ_SHUTDOWN);
+			}else if(mode == MODE_DRIVE){
+				mode = MODE_SHUTDOWN;
+				return DriveStep(input, output, state, REQ_SHUTDOWN);
+			}else if(mode == MODE_INIT){
+				mode = MODE_OFF;
 				return ERROR_NONE;
-				
+			}else if(mode == MODE_SHUTDOWN){
+				mode = MODE_OFF;
+				return ShutDownStep(input, output, state, REQ_OFF);
+			}else{
+				mode = MODE_OFF;
+				return FailStep(input, output, state, REQ_OFF);
 			}
 		}
-	}
+	}else if(input->keymodes == KEYMODE_ACCESSORIES)
 		
 
 }
