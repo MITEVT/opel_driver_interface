b08ba620680e6a474fc38b1b5f4ce8c5555a66fc
Skanda Koppula <skanda.koppula@gmail.com>
2015-12-19 23:33:25 -0500

    remove fail state fsm

--- firmware/src/fail.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "types.h"
-#include "fail.h"
-#include "util.h"
-
-ERROR_T FailStep(INPUT_T *input, OUTPUT_T *output, STATE_T *state, MODE_REQUEST_T mode_request){
-    return NULL;
-}
