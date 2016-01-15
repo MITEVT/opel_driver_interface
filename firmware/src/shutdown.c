fd32795107e8fea62be02bd657a23efaabb2c5aa
Bryson <bgalapon@mit.edu>
2016-01-14 21:42:04 -0500

    filled out the output stuff for shutdown.c

--- firmware/src/shutdown.c
+++ firmware/src/shutdown.c
@@ -5,12 +5,33 @@
 ERROR_T Shutdown_Step(INPUT_T *input, OUTPUT_T *output, STATE_T *state, MODE_REQUEST_T mode_request){
     
     ACCESSORIES_OUTPUT_REQUEST_T *acc_out = convert_acc(input->acc_input);
-    // TODO Change the below case details to correspond to shutdown state
-    //      (everything currently copied from Accessories SM)
-    //      Make sure to return error when velocity is not zero and shutdown is attempted
+        if(mode_request == REQ_OFF) {
+	INPUT_MESSAGES inp_messages = input->messages;
+	
+	bool BMS_hb = inp_messages->BMS_heartbeat;
+	bool PDM_hb = inp_messages->PDM_heartbeat;
+	bool throttle_hb = inp_messages->throttle_heartbeat;
+	uint16_t vel  = inp_messages->velocity;
+ 	
+	//returns error when velocity is not zero & shutdown is attempted
+	if(vel != 0){
+	    return ERROR_VELOCITY_NOT_ZERO;
+	}
+	
+	//follow the shutdown sequence
+	output->close_contactors = false;
+	output->acc_output = acc_out;
 
-    if(mode_request == REQ_OFF) {
-        //follow the shutdown sequence
+	OUTPUT_MESSAGES_T out_messages;
+	out_messages.test = false;
+	out_messages.send_heartbeat = false;
+	out_messages.drive_mode = MESSAGE_PARKED_AUX;
+	output->messages = &out_messages;
+	
+	//If adding FAIL state mode, then add a possibility of entering the
+	//fail state mode here.
+	state->dsm_mode = (!BMS_hb && !PDM_hb && !throttle_hb) ? MODE_OFF : MODE_SHUTDOWN;
+	return ERROR_NONE;
     }else{
 	//shouldn't get a request other than to turn off the car.
         return ERROR_ILLEGAL_STATE_REQUEST;
