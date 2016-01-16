c0732ecc89372000778c9d2dc3b0dc6241d0b9c2
Bryson <bgalapon@mit.edu>
2016-01-16 14:18:17 -0500

    implemented direction switching when velocity != 0 and shutdown logic when velocity != 0

--- firmware/src/drive.c
+++ firmware/src/drive.c
@@ -5,6 +5,8 @@
 ERROR_T Drive_Step(INPUT_T *input, OUTPUT_T *output, STATE_T *state, MODE_REQUEST_T mode_request){
     
     ACCESSORIES_OUTPUT_REQUEST_T *acc_out = convert_acc(input->acc_input);
+    DRIVE_DIRECTION_T dir = input->direction;
+    uint16_t vel = input->messages->velocity;
     // TODO Change the below case details to correspond to drive state
     //      (everything currently copied from Accessories SM)
 
@@ -43,9 +45,34 @@ ERROR_T Drive_Step(INPUT_T *input, OUTPUT_T *output, STATE_T *state, MODE_REQUES
         output->acc_output = acc_out;
 
         if(input->direction == DRIVE_FORWARD) {
-            out_messages.drive_mode = MESSAGE_DRIVE_FORWARD;
+
+	    if(state->direction != DRIVE_FORWARD){
+		if(vel == 0){
+		    out_messages.drive_mode = MESSAGE_DRIVE_FORWARD;
+		    state->direction = DRIVE_FORWARD;
+		}else{
+		    return ERROR_VELOCITY_NOT_ZERO;
+		}
+	    }else{
+ 		out_messages.drive_mode = MESSAGE_DRIVE_FORWARD;
+		state->direction = DRIVE_FORWARD;	    
+	    }
+           
         } else {
-            out_messages.drive_mode = MESSAGE_DRIVE_REVERSE;
+ 
+	    if(state->direction != DRIVE_REVERSE){
+		if(vel == 0){
+		    out_messages.drive_mode = MESSAGE_DRIVE_REVERSE;
+		    state->direction = DRIVE_REVERSE;
+		}else{
+		    return ERROR_VELOCITY_NOT_ZERO;
+		}
+	    }else{
+ 		out_messages.drive_mode = MESSAGE_DRIVE_REVERSE;
+		state->direction = DRIVE_REVERSE;	    
+	    }
+           
+           
         }
 
         OUTPUT_MESSAGES_T out_messages;
@@ -57,16 +84,19 @@ ERROR_T Drive_Step(INPUT_T *input, OUTPUT_T *output, STATE_T *state, MODE_REQUES
 	    return ERROR_NONE;
 
     } else if(mode_request == REQ_SHUTDOWN) {
-        output->close_contactors = true;
-        output->acc_output = turn_all_off();
 
+        output->close_contactors = true;
+        output->acc_output = (vel == 0) ? turn_all_off() : acc_out;
+		
         OUTPUT_MESSAGES_T out_messages;
         out_messages.test = false;
         out_messages.send_heartbeat = true;
-        out_messages.drive_mode = MESSAGE_SHUTDOWN_IMPENDING;
+
+	MESSAGE_DRIVE_MODE_T current_drive_mode = (vel > 0) ? MESSAGE_DRIVE_FORWARD : MESSAGE_DRIVE_REVERSE;
+        out_messages.drive_mode = (vel == 0) ? MESSAGE_SHUTDOWN_IMPENDING : current_drive_mode;
         output->messages = &out_messages;
 
-        state->dsm_mode = MODE_SHUTDOWN;
+        state->dsm_mode = (vel == 0) ? MODE_SHUTDOWN : MODE_DRIVE;
 	    return ERROR_NONE;
 
     } else if(mode_request == REQ_OFF) {
