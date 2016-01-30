8ee210dbf2c2540cb209a2c9a77904dc9ebe52fe
Eric Ponce <eaponce@mit.edu>
2016-01-30 12:23:45 -0500

    Fixed documentation problem
    There needs to be two asterisks in the first line of the comment. See commit changes for better detail

--- firmware/inc/init.h
+++ firmware/inc/init.h
@@ -11,7 +11,7 @@
 #include "stdint.h"
 #include "stdbool.h"
 
-/*
+/**
  * @details defines the threshold wait time for which we should
  * hear back from all the heartbeats, BMS precharge, and PDM checks
  */
@@ -21,7 +21,7 @@ typedef struct {
     uint32_t threshold_wait_time_pdm_ms;
 } Init_Config_T;
 
-/*
+/**
  * @details sets any initialization state configuration constants
  * (e.g. the threshold wait times)
  *
@@ -30,13 +30,13 @@ typedef struct {
  */
 void Init_Config(Init_Config_T init_config);
 
-/*
+/**
  * @details tears down any state that the initialization sub-machine used
  * (e.g. start wait times for tests)
  */
 ERROR INIT_Cleanup(STATE *state);
 
-/*
+/**
  * @details executes the magic of the initialization procedure
  * see the PDF in the opel_driver_interface repository /docs folder
  * for pseudocode outline
